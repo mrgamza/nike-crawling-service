2bfde3e93ac5f15991e4e67e553dccef509f1031
Donghyuk <mrgamza@gmail.com>
2023-05-27 12:01:23 +0900

    Fix date parsing error

--- nike_crawling_service/parser/Parser230514.py
+++ nike_crawling_service/parser/Parser230514.py
@@ -1,3 +1,5 @@
+import traceback
+
 from bs4 import BeautifulSoup
 from nike_crawling_service.util import DateUtil
 from nike_crawling_service.util import HTMLUtil
@@ -21,32 +23,53 @@ class Parser230514:
 
         results = []
 
-        for product in products:
-            draw_pdp = self.__get_pdp(product)
-            if draw_pdp is None:
-                continue
-
+        for index, product in enumerate(products):
             try:
+                draw_pdp = self.__get_pdp(product)
+                if draw_pdp is None:
+                    continue
+            
                 soup = BeautifulSoup(draw_pdp['response'], 'html.parser')
-                inner = soup.find('div', attrs={'class': 'product-info ncss-col-sm-12 full'})
-                name = inner.find('h1', attrs={'class': 'headline-5 pb3-sm'}).text
-                description = inner.find('h2', attrs={'class': 'headline-1 pb3-sm'}).text
-                price = inner.find('div', attrs={'class': 'headline-5 pb6-sm fs14-sm fs16-md'}).text
+                inner = soup.find('div', attrs={'class': 'product-info ncss-col-sm-12 full ta-sm-c'})
+                if inner is None:
+                    inner = soup.find('div', attrs={'class': 'product-info ncss-col-sm-12 full'})
+                    name = inner.find('h1', attrs={'class': 'headline-5 pb3-sm'}).text
+                    description = inner.find('h2', attrs={'class': 'headline-1 pb3-sm'}).text
+                    price = inner.find('div', attrs={'class': 'headline-5 pb6-sm fs14-sm fs16-md'}).text
+                else:
+                    name = inner.find('h1', attrs={'class': 'headline-5=small'}).text
+                    description = inner.find('h2', attrs={'class': 'headline-2'}).text
+                    price = inner.find('div', attrs={'class': 'headline-5 pb6-sm'}).text
+                
                 date_time_html = inner.find('div', attrs={'class': 'available-date-component'})
                 if date_time_html is None:
-                    is_same_date = False
-                else:
-                    date_time = self.__get_datetime(date_time_html.text)    
+                    date_time = draw_pdp['date_time']
                     is_same_date = DateUtil.is_same_date(request_datetime_kst, date_time)
+                    is_time_check = False
+                else:
+                    date_time = self.__get_datetime_pdp(date_time_html.text)
+                    is_same_date = DateUtil.is_same_date(request_datetime_kst, date_time)
+                    is_time_check = True
+                    
+                print("### Find product", index)
+                print("# Name : ", f'{name} - {description}')
+                print("# Link : ", draw_pdp['link'])
+                print("# Price : ", price)
+                print("# Date : ", date_time)
+                print("# Draw : ", draw_pdp['draw'])
                 
                 if is_same_date is False:
                     continue
-
-                is_same_time = True if time is None else \
-                    DateUtil.is_same_hour(request_datetime_kst, date_time)
+                
+                if is_time_check is True:
+                    is_same_time = True if time is None else \
+                        DateUtil.is_same_hour(request_datetime_kst, date_time)
+                else:
+                    is_same_time = True
+                
                 if is_same_time is False:
                     continue
-
+                
                 results.append({
                     'name': f'{name} - {description}',
                     'link': draw_pdp['link'],
@@ -55,6 +78,8 @@ class Parser230514:
                     "draw": draw_pdp['draw']
                 })
             except Exception as exception:
+                print("### Error product index", index)
+                print(traceback.format_exc())
                 raise exception
         return results
 
@@ -69,19 +94,41 @@ class Parser230514:
         response_text = response.text
         find = response_text.find('Draw로 출시됩니다')
         is_draw = find != -1
+        date_time = self.__get_datetime_list(product)
+        
         return {
             'response': response_text,
             'link': link,
+            'date_time': date_time,
             'draw': is_draw
         }
+        
+    # noinspection PyMethodMayBeStatic
+    def __get_datetime_list(self, product):
+        if product is None:
+            return None
+
+        date_find = product.find('div', attrs={'class': 'product-card ncss-row mr0-sm ml0-sm'})
+        date_find = date_find.find('div', attrs={'class': 'ncss-col-sm-12 full'})
+        date_find = date_find.find('a', attrs={'class': 'card-link d-sm-b'})
+        date_find = date_find.find('div', attrs={'class': 'launch-time ta-sm-l d-sm-h d-md-b z10 mod-bg-grey pt6-sm pl6-sm'})
+        date_find = date_find.find('div', attrs={'class': 'launch-caption ta-sm-c'})
+    
+        month = date_find.find('p', attrs={'class': 'headline-4'}).text.replace('월', '')
+        day = date_find.find('p', attrs={'class': 'headline-1'}).text.replace('일', '')
+        date_time = datetime(datetime.now().year, int(month), int(day))
+        
+        return date_time
+    
 
     # noinspection PyMethodMayBeStatic
-    def __get_datetime(self, date_text):
+    def __get_datetime_pdp(self, date_text):
         if date_text is None:
             return None
 
         transform_text = date_text.replace("출시", "").split(' ')[0: 3]
         date_str = ' '.join(transform_text).replace('오전', 'AM').replace('오후', 'PM')
+        timezone_kst = timezone(timedelta(hours=9))
         date_time_obj = datetime.strptime(date_str, '%m/%d %p %I:%M')
         date_time_obj = date_time_obj.replace(year=datetime.now().year,
                                               hour=(date_time_obj.hour + 9))
